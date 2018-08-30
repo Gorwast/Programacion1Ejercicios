ba63cfd272902e4e096576a76b01293f88b3139f
dii <dii@5J203-PCi.uson.mx>
2018-08-29 17:24:59 -0700

    Clase 1

---
+++ src/paquete1/TiposDatos2.java
@@ -0,0 +1,12 @@
+
+package paquete1;
+
+public class TiposDatos2 {
+
+    public static void main(String[] args) {
+        String cadena;
+        char c = '!';
+        cadena = "esta es una cadena";
+        System.out.println(cadena);
+    }
+}
