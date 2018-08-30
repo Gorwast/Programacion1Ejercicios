886c8674ee9e7b2c2fa2f7caa0758b78cd6c6c99
gorwast <lk23@live.com.mx>
2018-08-29 17:48:34 -0700

    Clase 1

--- src/paquete1/TiposDatos2.java
+++ src/paquete1/TiposDatos2.java
@@ -5,8 +5,18 @@ public class TiposDatos2 {
 
     public static void main(String[] args) {
         String cadena;
-        char c = '!';
+        char c;
+        c = 'e';
+        
         cadena = "esta es una cadena";
+        System.out.println(cadena);
+        System.out.print(cadena.charAt(0));
+        System.out.print(cadena.charAt(1));
+        System.out.print(cadena.charAt(2));
+        System.out.print(cadena.charAt(3));
+        System.out.print(cadena.charAt(4));
+        System.out.print(cadena.charAt(5) + "\n");
+        cadena = String.valueOf(c);
         System.out.println(cadena);
     }
 }
