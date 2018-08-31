4123f87c9558c9c29949694145bf58466eb3680c
gorwast <lk23@live.com.mx>
2018-08-30 17:46:10 -0700

    Clase 2

--- src/paquete1/Operadores.java
+++ src/paquete1/Operadores.java
@@ -1,23 +1,26 @@
-
 package paquete1;
+
 /*
  * @author dii
  */
 public class Operadores {
-    public static void main(String [] args){
-        int a,b,c;
+
+    public static void main(String[] args) {
+        int a, b, c, d, e;
         a = 70;
         b = 20;
-        c = a + b;
+        c = a + b; //suma
         System.out.println("a + b = " + c);
-        c = a - b;
+        c = a - b; //Resta
         System.out.println("a - b = " + c);
-        c = a * b;
+        c = a * b; //Multiplicacion
         System.out.println("a * b = " + c);
-        c = a / b;
+        c = a / b; //Division
         System.out.println("a / b = " + c);
-        System.out.println("c++ = " + ++c);
-        System.out.println();
-        
+        c++; //Aumentar
+        System.out.println("c++ = " + c);
+        c = a + b; //
+        System.out.println("a + b = " + c);
+        c = a + b; //SUM
     }
 }
